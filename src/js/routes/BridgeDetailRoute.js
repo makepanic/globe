0728d915013d79cf3d45d82462e7fb4808715877
Christian <me@rndm.de>
2013-11-07 20:41:55 +0100

    fixed bridge detail if nothing is found

--- src/js/routes/BridgeDetailRoute.js
+++ src/js/routes/BridgeDetailRoute.js
@@ -7,14 +7,19 @@ GLOBE.BridgeDetailRoute = Ember.Route.extend({
 
         GLOBE.OnionooDetail.find(fingerprint).then(function(item){
 
-            item = item.bridge;
-            controller.set('model', item);
+            // check if found bridge
+            if (item.bridge.hasOwnProperty('hashed_fingerprint')) {
 
-            // no weight data
-            GLOBE.OnionooBandwidthHistory.find(fingerprint, true).then(function(data){
-                controller.set('bandwidthPeriods', data.bridges.periods);
-                controller.set('bandwidthData', data.bridges.history);
-            });
+                item = item.bridge;
+                controller.set('model', item);
+
+                // no weight data
+                GLOBE.OnionooBandwidthHistory.find(fingerprint, true).then(function(data){
+                    controller.set('bandwidthPeriods', data.bridges.periods);
+                    controller.set('bandwidthData', data.bridges.history);
+                });
+
+            }
         });
     }
 });
