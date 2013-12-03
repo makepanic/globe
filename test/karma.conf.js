7eebb17dff5dbdc366c0bc5dfd4a541627bd83a6
Christian <me@rndm.de>
2013-12-03 22:24:02 +0100

    reverted karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,68 +1,61 @@
-// Karma configuration
-// Generated on Sat Aug 03 2013 21:23:56 GMT+0200 (CEST)
+/*global module*/
+module.exports = function(config) {
+    config.set({
+        // Karma configuration
 
+        // base path, that will be used to resolve files and exclude
+        basePath: '..',
 
-// base path, that will be used to resolve files and exclude
-basePath = '..';
+        frameworks : ['qunit'],
 
-// list of files / patterns to load in the browser
-files = [
-    QUNIT, QUNIT_ADAPTER,
-    'test/ember-test-bootstrap.js',
-    'build/dist/globe.0.4.1.min.js',
-    'test/unit/*.js',
-    'test/integration/*.js'
-    //{pattern: 'test/unit/*.js', watched: true, included: true, served: true}
-];
+        // list of files / patterns to load in the browser
+        files : [
+            'test/ember-test-bootstrap.js',
+            'build/dist/globe.0.4.1.min.js',
+            'test/unit/*.js',
+            'test/integration/*.js'
+        ],
 
+        // list of files to exclude
+        exclude : [
+        ],
 
-// list of files to exclude
-exclude = [
-  
-];
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit'
+        reporters : ['progress'],
 
+        // web server port
+        port : 9876,
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+        // cli runner port
+        runnerPort : 9100,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors : true,
 
-// web server port
-port = 9876;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel : config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch : true,
 
-// cli runner port
-runnerPort = 9100;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers : ['PhantomJS'],
 
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout : 60000,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun : true
 
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+    });
+};
