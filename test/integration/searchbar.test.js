c55d2a9ac3f8e26426250749be3fd1fab1590050
Christian <me@rndm.de>
2013-12-04 16:59:16 +0100

    #39 fixed failing test, updated grunt-ember-templates

--- test/integration/searchbar.test.js
+++ test/integration/searchbar.test.js
@@ -13,23 +13,17 @@ test('the advanced search should not be visible', function () {
     })
 });
 
-test('the advanced search should appear on button click', function () {
+test('the advanced search should appear and disappear on button click', function () {
     var advancedSearchButton = find('.toggle-advanced-search'),
         advancedSearch = find('.advanced-search-form');
 
     equal(advancedSearchButton.length, 1, 'has advanced search button exists');
 
-    click('.toggle-advanced-search');
+    click('.toggle-advanced-search').then(function () {
+        equal(advancedSearch[0].classList.contains('search-filters-enabled'), true, 'advanced search form is visible');
 
-    equal(advancedSearch[0].classList.contains('search-filters-enabled'), true, 'advanced search form is visible');
+        click('.toggle-advanced-search').then(function () {
+            equal(advancedSearch[0].classList.contains('search-filters-enabled'), false, 'advanced search form is not visible');
+        });
+    });
 });
-
-test('the advanced search should disappear on repeated button click', function () {
-    var advancedSearch = find('.advanced-search-form');
-
-    click('.toggle-advanced-search');
-    click('.toggle-advanced-search');
-
-    equal(advancedSearch[0].classList.contains('search-filters-enabled'), false, 'advanced search form is not visible');
-});
-
