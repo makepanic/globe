69d49d34390cd4fd4a3a7a427df19857c4ed6977
Christian <me@rndm.de>
2013-12-04 17:49:20 +0100

    fixed vagrant bootstrap for karma tests

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,10 +5,19 @@ apt-get update > /dev/null
  
 # install nodejs requirements (say yes to all)
 # via https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager#ubuntu-mint-elementary-os
-apt-get install -y python-software-properties python g++ make git
+# freetype and fontconfig for phantomjs
+apt-get install -y python-software-properties python g++ make git libfreetype6 libfontconfig1
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update > /dev/null
 apt-get -y install nodejs
+
+# install phantomjs 1.9.2
+cd /usr/local/share
+wget http://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-i686.tar.bz2
+tar xjf phantomjs-1.9.2-linux-i686.tar.bz2
+sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-i686/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-i686/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-i686/bin/phantomjs /usr/bin/phantomjs
  
 # clone globe
 cd /vagrant/
